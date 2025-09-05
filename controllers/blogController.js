832125b14ce6b3390e683e9cb330edd2c2707661
Captain <Captain@now>
2025-09-05 16:17:30 +0300

    Solved Vercel issues #17?

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -21,19 +21,21 @@ const blog_create_get = (req, res) => {
     res.render('create', { title: 'Story Telling' });
 }
 
-const blog_create_post = (req, res) => {
-    const blog = new Blog(req.body);
+const blog_create_post = async (req, res) => {
+    try {
+        const blog = new Blog(req.body);
+        await blog.save();  // wait for the blog to be saved
 
-    blog.save()
-        .then(() => Blog.find().sort({ createdAt: -1 }))
-        .then(blogs => {
-            // Directly render the "all blogs" page instead of redirecting
-            res.render('AllBloges', { title: 'Stories', blogs });
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send('Something went wrong while creating the blog.');
-        });
+        // Fetch all blogs sorted by newest first
+        const blogs = await Blog.find().sort({ createdAt: -1 });
+
+        // Render the "AllBloges" page directly
+        res.render('AllBloges', { title: 'Stories', blogs });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Something went wrong while creating the blog.');
+    }
 };
 
 const blog_details = (req, res) => {
